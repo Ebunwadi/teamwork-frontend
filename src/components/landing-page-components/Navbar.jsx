479429ab336534db408312b4f3113a2625fac2b7
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2022-12-18 22:27:39 +0100

    ft(media query): add media query

--- src/components/landing-page-components/Navbar.jsx
+++ src/components/landing-page-components/Navbar.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-// import '../../pages/landing-page/navbar.css'
+import React, {useState} from 'react'
 import styles from './navbar.module.css'
 import { Link } from 'react-router-dom'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { FaTimes } from 'react-icons/fa'
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={styles.navbar}>
       <span className={styles.staffcon}> STAFFCONN</span>
-      <Link to='/' className={styles.home}><span>Home</span></Link>
-      <Link to='/upload' className={styles.upload}><span>Upload</span></Link>
-      <Link to='/notify' className={styles.notify}><span>Notification</span></Link>
-      <Link to='/profile' className={styles.profile}><span>Profile</span></Link>
-      <Link to='/login' className={styles.login}><span className={styles.loginText}>Login</span></Link>
+      <div className={`${isOpen? styles.open : styles.navItems}`}>
+        <Link to='/' className={styles.home}><span>Home</span></Link>
+        <Link to='/upload' className={styles.upload}><span>Upload</span></Link>
+        <Link to='/notify' className={styles.notify}><span>Notification</span></Link>
+        <Link to='/profile' className={styles.profile}><span>Profile</span></Link>
+        <Link to='/login' className={styles.login}><span className={styles.loginText}>Login</span></Link>
+      </div>
+      <button 
+        onClick={() => {setIsOpen(!isOpen)}} 
+        className = {styles.button}> {isOpen? <FaTimes /> : <GiHamburgerMenu />}
+      </button>
     </div>
   )
 }
