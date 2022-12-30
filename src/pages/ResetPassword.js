9a2d202c7523eee887720252fceca83b4c485b74
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2022-12-30 20:14:29 +0100

    ft(functional login page): implement login and forgot password functionality

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -4,19 +4,19 @@ import { useSelector } from "react-redux";
 
 
 function ResetPassword() {
-  const token = useSelector((state) => state.auth.token)
-  const id = useSelector((state) => state.auth.user)
-  const fetchUser = async () => {
-    await fetch(`https://ebubeproject.onrender.com/api/v1/auth/reset-password/${id}/${token}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-  }
-  useEffect(() => {
-    fetchUser();
-// eslint-disable-next-line
-  }, []);
+//   const token = useSelector((state) => state.auth.token)
+//   const id = useSelector((state) => state.auth.user)
+//   const fetchUser = async () => {
+//     await fetch(`https://ebubeproject.onrender.com/api/v1/auth/reset-password/${id}/${token}`, {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     })
+//   }
+//   useEffect(() => {
+//     fetchUser();
+// // eslint-disable-next-line
+//   }, []);
   return (
     <div>
       <Password />
