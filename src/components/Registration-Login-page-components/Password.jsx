b27d3dd5b4a244dc4c0153474e781b51dad3f832
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2023-01-03 23:35:18 +0100

    ft(functional login page): implement login and forgot password functionality

--- src/components/Registration-Login-page-components/Password.jsx
+++ src/components/Registration-Login-page-components/Password.jsx
@@ -4,13 +4,14 @@ import {RiLockPasswordLine} from 'react-icons/ri'
 import {AiOutlineEye} from 'react-icons/ai'
 import {AiOutlineEyeInvisible} from 'react-icons/ai'
 import {useNavigate, useParams} from "react-router-dom";
-import { useForm } from "react-hook-form";
+// import { useSelector } from "react-redux";
 
 function Password() {
+  // const token = useSelector((state) => state.auth.token)
+  // const id = useSelector((state) => state.auth.user)
   const navigate = useNavigate();
-  const params = useParams();
+  let params = useParams();
   const [isShown, setIsShown] = useState(false);
-  const [isPasswordShown, setIsPasswordShown] = useState(false);
   const initialForm = {
     password: '',
     confirmPassword: ''
@@ -22,23 +23,16 @@ function Password() {
       [e.target.name] : e.target.value
   })
   }
-
-  // handle form events
-//   const { register, watch, formState: { errors } } = useForm({
-//     mode:'onTouched'
-// });
-
-// const password = watch('password')
-
   const handleSubmit = async (e) => {
     e.preventDefault()
       const res = await fetch(`https://ebubeproject.onrender.com/api/v1/auth/reset-password/${params.id}/${params.token}`, {
-      method: "POST",
+      method: "PATCH",
       body: JSON.stringify(form),
       headers: {
         "content-type": "application/json",
       },
     });
+  // console.log(token, id);
 
     const feedback = await res.json();
     console.log(feedback);
@@ -60,27 +54,13 @@ function Password() {
         <div className={styles.input}>
           <input 
             type={isShown? 'text': 'password'} 
-            placeholder="Enter your new password" 
+            placeholder="Enter your new passwoerd" 
             className={styles.email} 
             autoComplete = 'off'
             name='password'
             value={form.password}
             onChange={handleChange}
-            // {...register("password", { required: 'Password is required',
-              // pattern:{
-                // eslint-disable-next-line
-                // value:/^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{10,16}$/,
-                // message:'Password should include at least one uppercase, one numeric value and one special character'
-            //   },
-            //   minLength:{
-            //     value:4,
-            //     message:'Minimum Required length is 4'
-            //   },
-            //   maxLength: {
-            //     value: 20,
-            //     message: "Maximum Required length is 20",
-            //     },
-            // })}
+            required
           />
           <RiLockPasswordLine className={styles.passwordIcon} />
           <div
@@ -88,38 +68,24 @@ function Password() {
             className = {styles.eyeButton}> {isShown? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
           </div>
         </div>
-        <br /><br /><br />
-        {/* {errors.password && <span className={styles.passwordError}>{errors.password.message}</span>} */}
-          <br /><br />
-          {/* confirm password */}
+          <br /><br /> <br /><br />
         <div className={styles.input}>
           <input 
-            type={isPasswordShown? 'text': 'password'} 
+            type={isShown? 'text': 'password'} 
             placeholder="Confirm Password" 
             className={styles.password} 
             autoComplete = 'off'
             name='password'
             value={form.confirmPassword}
             onChange={handleChange}
-            onPaste={(e)=>{
-              e.preventDefault()
-              return false;
-            }}
-            // {...register("confirmPassword", { required: 'confirm password is required',
-            //   validate: (value) =>
-            //   value === password || "The passwords do not match",
-            // })}
           />
           <RiLockPasswordLine className={styles.passwordIcon} />
           <div
-            onClick={() => {setIsPasswordShown(!isPasswordShown)}} 
-            className = {styles.eyeButton}> {isPasswordShown? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+            onClick={() => {setIsShown(!isShown)}} 
+            className = {styles.eyeButton}> {isShown? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
           </div>
         </div>
-        <br /><br /><br />
-        {/* {errors.confirmPassword && <span className={styles.passwordError}>{errors.confirmPassword.message}</span>} */}
-        <br /><br /><br />
-        <button className={`${styles.input} ${styles.btnn}`} type = 'submit'>Reset Password</button>
+        <button className={`${styles.input} ${styles.btn}`} type = 'submit'>Reset Password</button>
       </form>
     </div>
   )
